5c842b658efc9a41cbe1ed51dc627844caf8331e
200~ShivanandThakur-2002 <Shivanandthakur2002@gmail.com>
2024-12-23 11:13:35 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
-// const cors = require('cors'); // Import cors
+const cors = require('cors'); // Import cors
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 
@@ -13,7 +13,7 @@ const port = 3000;
 
 ///THEHRIOER
 // const corsOpts = {
-//     origin: '*',
+//     origin: '*',git
   
 //     methods: [
 //       'GET',
