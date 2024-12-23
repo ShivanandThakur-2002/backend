34e1d140f30cc648f5a9b11e08e597dea4620354
200~ShivanandThakur-2002 <Shivanandthakur2002@gmail.com>
2024-12-23 14:51:59 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 require('dotenv').config();
 
